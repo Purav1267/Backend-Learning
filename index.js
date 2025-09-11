8f8438776617af52583de6b8281c0be1a44e133d
PURAV <puravmalik24@gmail.com>
2025-09-12 00:17:19 +0530

    Here i learend about how to import express.js so first we have to write const express = require(express) , and then const app = express() , we learned about how to import dotenv file into the index.js , and then we learend about how to use app.get() and app.listen and many more

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+require('dotenv').config()
 console.log("PURAV - Chai aur Code");
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send("puravmalik24")
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1> Please login at chai aur code </h1>')
+})
+
+app.get('/youtube',(req,res)=>{
+    res.send("<h2>ElectroRyzen</h2>")
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Example app listening on port ${process.env.PORT}`);
+})
