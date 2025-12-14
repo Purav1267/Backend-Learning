a0fa00d0b15f2a05054d04b3c7855d991dc3cb09
Purav1267 <puravmalik24@gmail.com>
2025-12-14 18:18:40 +0530

    Learned about middleware hooks , models , checking through models , bcrypt , jwt , jwt.sign() , bcrypt.hash(), plugin of aggregate pagination so that we can push our models code or unique code to the database , and learned about when you want to extract values from the code above with this statement then you can't use arrow functions you have to go with basic functions , and did learned about both ACCESS TOKEN AND REFRESH TOKEN so to write code for it as a middleware methods.

--- 02_backend/src/models/user.model.js
+++ 02_backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -42,5 +44,50 @@ const userSchema = new mongoose.Schema({
     } 
 
 },{timestamps: true})
+
+// this pre is a Middleware hook
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next()
+
+    this.password = bcrypt.hash(this.password , 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+// we can;t use arrow functions here cause it doesnt have the context of this . 
+// as we want the this password to be encrypted before saving and arrow function doesn't help us in that 
+// that why using basic function here.
+
+
+// So we are going with sessions and cookies. So to access the cokkie you want the accesstoken but to access the session you want the refresh token.
+// we will be storing the refresh token in the database.
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+    {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User",userSchema)
